8a0ef1073ca1cbc56dda58bbce236e76af08244f
Michael Kwaku Aboagye <35949773+Rev0kz@users.noreply.github.com>
2018-12-10 15:00:19 +0000

    Update and rename loginform.py to Registerform.py

--- Registerform.py
+++ Registerform.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, 
 from wtforms.validators import DataRequired
 
-class Login(FlaskForm):  
+class RegisterForm(FlaskForm):  
   name = StringField('name', validators=[DataRequired(), Length(max=255)]) 
   password = PasswordField('password', validators=[DataRequired()])  
   
