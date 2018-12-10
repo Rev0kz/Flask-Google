f1c9607213df25615e15f539e0fce63fd6c7df51
Michael Kwaku Aboagye <35949773+Rev0kz@users.noreply.github.com>
2018-12-10 16:25:54 +0000

    Update Registerform.py

--- Registerform.py
+++ Registerform.py
@@ -3,8 +3,8 @@ from wtforms import StringField, PasswordField,
 from wtforms.validators import DataRequired
 
 class RegisterForm(FlaskForm):  
-  name = StringField('name', validators=[DataRequired(), Length(max=255)]) 
-  password = PasswordField('password', validators=[DataRequired()])  
+  name = StringField('name', [validator.DataRequired(), Length(max=255)]) 
+  password = PasswordField('new password', [validator.DataRequired()])  
   
      
           
