93a683fa567bdf24066992393cd480164260edd6
Michael Kwaku Aboagye <35949773+Rev0kz@users.noreply.github.com>
2018-12-11 12:40:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app =  Fask(app)
 
 
 @app.route('/register', methods=['GET', 'POST'])
-  form = Registerform()
+  form = Registerform(request.method)
   if request.method == 'POST' and form.validate()
     user = User(form.user.data, form.password.data, form.email.data)
     flash("successful registration")
-    return redirect(url_for("login"))
+    return redirect(url_for("googlemap"))
   return render_template("register.html", form=form)
     
 
