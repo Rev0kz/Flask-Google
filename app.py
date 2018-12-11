dcd7c345675e3895cb894d292f62ee8e73cb5607
Michael Kwaku Aboagye <35949773+Rev0kz@users.noreply.github.com>
2018-12-11 12:30:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask , render_template 
-from Login import LoginForm   
+from Login import Registerform   
 
 app =  Fask(app)   
 
 
 @app.route('/register', methods=['GET', 'POST'])
-  form = 
+  form = Registerform()
+  if request.method == 'POST' and form.validate()
+    user = User(form.user.data, form.password.data, form.email.data)
+    flash("successful registration")
+    return redirect(url_for("login"))
+  return render_template("register.html", form=form)
+    
 
 
 if __name__ = '__main__':
