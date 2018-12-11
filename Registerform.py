8684269e495a03be37c39dc8a554979aa1db0079
Michael Kwaku Aboagye <35949773+Rev0kz@users.noreply.github.com>
2018-12-11 12:00:14 +0000

    Update Registerform.py

--- Registerform.py
+++ Registerform.py
@@ -3,8 +3,9 @@ from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, EqualTo, Length, 
 
 class RegisterForm(FlaskForm):  
-  name = StringField('name', [validator.DataRequired(), Length(max=255)]) 
-  password = PasswordField('new password', [validator.DataRequired(),validators.EqaulTo('confirm', message='password must match']) 
+  name = StringField('name', [validator.DataRequired(), validators.Length(max=255)]) 
+  password = PasswordField('new password', [validator.DataRequired(),validator.Length(min=8),
+                                            validators.EqaulTo('confirm', message='password must match']) 
   confirm = PasswordField('Repeat Password')                                        
   
      
