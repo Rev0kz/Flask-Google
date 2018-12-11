a5f698b3eb91e0e632c177606c4f440ef28336fe
Michael Kwaku Aboagye <35949773+Rev0kz@users.noreply.github.com>
2018-12-11 11:32:56 +0000

    Update Registerform.py

--- Registerform.py
+++ Registerform.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm 
-from wtforms import StringField, PasswordField, 
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, EqualTo, Length, 
 
 class RegisterForm(FlaskForm):  
   name = StringField('name', [validator.DataRequired(), Length(max=255)]) 
-  password = PasswordField('new password', [validator.DataRequired()])  
+  password = PasswordField('new password', [validator.DataRequired(),validators.EqaulTo('confirm', message='password must match']) 
+  confirm = PasswordField('Repeat Password')                                        
   
      
           
