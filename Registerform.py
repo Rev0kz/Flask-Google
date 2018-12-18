fb876c5227d87b9f58350ac4b32773b03863c58e
Michael Kwaku Aboagye <35949773+Rev0kz@users.noreply.github.com>
2018-12-18 15:42:25 +0000

    Update Registerform.py

--- Registerform.py
+++ Registerform.py
@@ -1,13 +1,14 @@
-from flask_wtf import FlaskForm 
-from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import DataRequired, EqualTo, Length, 
+from wtforms import StringField, PasswordField, BooleanField, Form, validators
+from flask_wtf.recaptcha import RecaptchaField
 
-class RegisterForm(FlaskForm):  
-  name = StringField('name', [validator.DataRequired(), validators.Length(max=255)]) 
-  password = PasswordField('new password', [validator.DataRequired(),validator.Length(min=8),
-                                            validators.EqaulTo('confirm', message='password must match']) 
+class RegisterForm(Form):  
+  name = StringField('name', [validators.DataRequired(), validators.Length(max=255)]) 
+  password = PasswordField('new password', [validators.DataRequired(),validators.Length(min=8),
+                                            validators.EqualTo('confirm', message='password must match')]) 
   confirm = PasswordField('Repeat Password')     
-  email = StringField('emailaddress', [validator.DataRequired(), validator.Length(min=6, max=35)] 
+  email = StringField('emailaddress', [validators.DataRequired(), validators.Length(min=6, max=35)]) 
+
+  recaptcha = RecaptchaField()
   
      
           
